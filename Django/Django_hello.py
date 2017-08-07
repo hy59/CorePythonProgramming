1711b4fb9b0cff5877f5df6e7fcd23381050bb8f
adam huang <937162905@qq.com>
2017-08-07 22:32:33 +0800

    add django project hello.py

--- Django/Django_hello.py
+++ Django/Django_hello.py
@@ -1,8 +1,34 @@
 # -*- coding: utf-8 -*-
 
+import sys
 from django.http import HttpResponse
+from django.conf.urls import url
+from django.conf import settings
+from django.core.management import execute_from_command_line
 
+
+settings.configure(
+    DEBUG = True,
+    SECRET_KEY = 'thisisthesecretkey',
+    ROOT_URLCONF = __name__,
+    MIDDLEWARE_CLASSES = (
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
 
 
 def index(request):
     return HttpResponse('Hello World')
+
+
+urlpatterns = (
+    url(r'^$', index)
+)
+
+
+if __name__ == '__main__':
+    execute_from_command_line(sys.argv)
+    
+
