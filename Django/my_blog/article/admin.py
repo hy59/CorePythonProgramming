891aa37419ea521a0d6b6913453698e4db90e4e1
adam huang <937162905@qq.com>
2017-08-14 16:13:58 +0800

    fix the migration error

--- Django/my_blog/article/admin.py
+++ Django/my_blog/article/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article
+
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'date_time',)
+
+
+admin.site.register(Article, ArticleAdmin)
