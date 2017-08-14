d082710fd66027544092b6b77a48f5e97cb2ef5f
adam huang <937162905@qq.com>
2017-08-14 16:38:49 +0800

    add a view and test it

--- Django/my_blog/article/views.py
+++ Django/my_blog/article/views.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Welcome!")
+
+def detail(request, my_args):
+    post = Article.objects.all()[int(my_args)]
+    str = ('title = %s, category = %s, date_time = %s, content = %s'
+            % (post.title, post.category, post.date_time, post.content))
+    return HttpResponse(str)
--- Django/my_blog/my_blog/urls.py
+++ Django/my_blog/my_blog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from article import views as article_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', article_views.home, name='home'),
+    url(r'^(?P<my_args>\d+)/$', article_views.detail, name='detail'),
 ]
