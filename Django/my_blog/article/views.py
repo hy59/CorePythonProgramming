9689d98487feaed1940cbfaf66924958a63a7764
adam huang <937162905@qq.com>
2017-08-14 22:14:40 +0800

    use active URL

--- Django/my_blog/article/views.py
+++ Django/my_blog/article/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
+from datetime import datetime
+from django.http import Http404
 
 # Create your views here.
 def home(request):
@@ -9,7 +11,6 @@ def home(request):
     return render(request, 'home.html', {'post_list': post_list})
 
 def detail(request, my_args):
-    post = Article.objects.all()[int(my_args)]
-    str = ('title = %s, category = %s, date_time = %s, content = %s'
-            % (post.title, post.category, post.date_time, post.content))
-    return HttpResponse(str)
+    print (my_args)
+    post = Article.objects.get(id=int(my_args))
+    return render(request, 'post.html', {'post': post})
